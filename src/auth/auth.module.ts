82c183cf3ea919fdd63b8e877701e5cbbced0e68
unknown <Joo Victor>
2022-10-26 20:27:52 -0300

    New Updates:
    Updating the AuthModule (new imports and providers were added)

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,22 @@
 import { Module } from "@nestjs/common";
+import { JwtModule } from "@nestjs/jwt/dist";
+import { PassportModule } from "@nestjs/passport";
+import { CustumerModule } from "src/customer/costumer.module";
 import { Bcrypt } from "./bcrypt/bcrypt";
+import { jwtConstants } from "./constants/constants";
+import { AuthService } from "./services/auth.service";
+import { LocalStrategy } from "./strategy/local.strategy";
 
 @Module({
-    imports: [],
-    providers: [Bcrypt],
+    imports: [
+        CustumerModule,
+        PassportModule,
+        JwtModule.register({
+            secret: jwtConstants.secret,
+            signOptions: {expiresIn: '24h'},
+        })
+    ],
+    providers: [Bcrypt, AuthService, LocalStrategy],
     controllers: [],
     exports: [Bcrypt],
 })
